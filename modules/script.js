da5687aaeb6cad4c521ab7c15292b442c90fa2e0
Ana <anawu.datawork@gmail.com>
2024-11-20 17:28:33 +0800

    update

--- modules/script.js
+++ modules/script.js
@@ -1,6 +1,41 @@
 const items = [
   {
-    id: "0",
+    id: 0,
+    name: "肥宅心碎賞櫻3日",
+    imgUrl:
+      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
+    area: "高雄",
+    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
+    group: 87,
+    price: 1400,
+    rate: 10,
+  },
+  {
+    id: 1,
+    name: "貓空纜車雙程票",
+    imgUrl:
+      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+    area: "台北",
+    description:
+      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
+    group: 99,
+    price: 240,
+    rate: 2,
+  },
+  {
+    id: 2,
+    name: "台中谷關溫泉會1日",
+    imgUrl:
+      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
+    area: "台中",
+    description:
+      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
+    group: 20,
+    price: 1765,
+    rate: 7,
+  },
+  {
+    id: 3,
     name: "綠島自由行套裝行程",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true",
@@ -12,7 +47,7 @@ const items = [
     group: 8,
   },
   {
-    id: "1",
+    id: 4,
     name: "清境高空觀景步道二日遊",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_4.png?raw=true",
@@ -24,7 +59,7 @@ const items = [
     group: 12,
   },
   {
-    id: "2",
+    id: 5,
     name: "南庄度假村露營車二日遊",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_6.png?raw=true",
@@ -36,7 +71,7 @@ const items = [
     group: 2,
   },
   {
-    id: "3",
+    id: 6,
     name: "山林悠遊雙人套票",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_3.png?raw=true",
@@ -48,7 +83,7 @@ const items = [
     group: 100,
   },
   {
-    id: "4",
+    id: 7,
     name: "漁樂碼頭釣魚體驗套票",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_2.png?raw=true",
@@ -60,7 +95,7 @@ const items = [
     group: 5,
   },
   {
-    id: "5",
+    id: 8,
     name: "熊森公園親子二日遊套票",
     imgUrl:
       "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_5.png?raw=true",
